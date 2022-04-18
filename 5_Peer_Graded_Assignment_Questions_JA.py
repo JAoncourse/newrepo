f060e45513177fb748164fccfccbd3f215bd6545
JAoncourse <103900228+JAoncourse@users.noreply.github.com>
2022-04-18 15:44:54 +0200

    Update 5_Peer_Graded_Assignment_Questions_JA.py
    Updated intendations

--- 5_Peer_Graded_Assignment_Questions_JA.py
+++ 5_Peer_Graded_Assignment_Questions_JA.py
@@ -76,7 +76,7 @@ app.layout = html.Div(children=[
                                 # Enter your code below. Make sure you have correct formatting.,
                                 app.layout = html.Div(children=[html.H1('US Domestic Airline Flights Performance', 
                                 style={'textAlign': 'center', 'color': '#503D36',
-                                'font-size': 24}),
+                                    'font-size': 24}),
     
                                 # REVIEW2: Dropdown creation,
                                  
@@ -93,16 +93,15 @@ app.layout = html.Div(children=[
                                         # TASK2: Add a dropdown
                                         # Enter your code below. Make sure you have correct formatting.
                                         dcc.Dropdown(id='input-type', 
-                                        options=[
-                                        {'label': 'Yearly Airline Performance Report', 'value': 'OPT1'},
-                                        {'label': 'Yearly Airline Delay Report', 'value': 'OPT2'},
-                                        
-                                        ],
-                                        placeholder='Select a report type
-                                        style={'width'=0.8, 'padding'=3, 'font-size': 20, textAlign': 'center',
-                                })
+                                                      options=[
+                                                         {'label': 'Yearly Airline Performance Report', 'value': 'OPT1'},
+                                                          {'label': 'Yearly Airline Delay Report', 'value': 'OPT2'}
+                                                           ],
+                                                      placeholder='Select a report type
+                                                      style={'width'=0.8, 'padding'=3, 'font-size': 20, textAlign': 'center',
+                                                      })
                                          
-                                        # Providing a vallue to dropdown
+                                        # Providing a value to dropdown
                                             ),
                                     # Place them next to each other using the division style
                                     ], style={'display':'flex'}),
@@ -131,7 +130,8 @@ app.layout = html.Div(children=[
                                 html.Div([
                                         html.Div([ ], id='plot2'),
                                         html.Div([ ], id='plot3')
-                                ], style={'display': 'flex'}),
+                                        ], 
+                                        style={'display': 'flex'}),
                                 
                                 # TASK3: Add a division with two empty divisions inside. See above disvision for example.
                                 # Enter your code below. Make sure you have correct formatting.
@@ -140,21 +140,24 @@ app.layout = html.Div(children=[
                                 html.Div([
                                         html.Div([ ], id='plot4'),
                                         html.Div([ ], id='plot5')
-                                ], style={'display': 'flex'})
-                                ])
+                                        ], 
+                                        style={'display': 'flex'})
+                                    ])
 
 # Callback function definition
 # TASK4: Add 5 ouput components
 # Enter your code below. Make sure you have correct formatting.,
-[Output(component_id='plot1', component_property='children'),
- Output(component_id='plot2',),
- Output(component_id='plot3',),
- Output(component_id='plot4',),
- Output(component_id='plot5',)]
+                [Output(component_id='plot1', component_property='children'),
+                 Output(component_id='plot2',component_property='children'),
+                 Output(component_id='plot3',component_property='children'),
+                 Output(component_id='plot4',component_property='children'),
+                 Output(component_id='plot5',component_property='children')
+                ]
 
 @app.callback( [....],
                [Input(component_id='input-type', component_property='value'),
                 Input(component_id='input-year', component_property='value')],
+              
                # REVIEW4: Holding output state till user enters all the form information. In this case, it will be chart type and year
                [State("plot1", 'children'), State("plot2", "children"),
                 State("plot3", "children"), State("plot4", "children"),
@@ -174,13 +177,11 @@ def get_graph(chart, year, children1, children2, c3, c4, c5):
             bar_fig = px.bar(bar_data, x='Month', y='Flights', color='CancellationCode', title='Monthly Flight Cancellation')
             
             # TASK5: Average flight time by reporting airline
-            # Enter your code below. Make sure you have correct formatting.,
+            # Enter your code below. Make sure you have correct formatting.
+            line_fig = px.line(line_data, x='Month', y='AirTime', 
+                               color='Reporting_Airline', 
+                               title='Average monthly flight time minutes by airline')
 
-
-line_fig = px.line(line_data, x='Month', y='AirTime', color='Reporting_Airline', title='Average monthly flight time minutes by airline')
-
-            
-            
             # Percentage of diverted airport landings per reporting airline
             pie_fig = px.pie(div_data, values='Flights', names='Reporting_Airline', title='% of flights by reporting airline')
             
@@ -203,7 +204,7 @@ line_fig = px.line(line_data, x='Month', y='AirTime', color='Reporting_Airline',
                       color='Flights',
                       color_continuous_scale='RdBu',
                       title='Flight count by airline to destination state'
-                )
+                                 )
             
             
             # REVIEW6: Return dcc.Graph component to the empty division
